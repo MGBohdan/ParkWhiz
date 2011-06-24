312c73335dc8194fd3bb380c2a387ae6b108171e
bbsoft <bbsoft@live.com>
2011-06-25 00:29:41 +0300

    Initial commit

---
+++ src/org/bbsoft/ParkWhiz/ParkWhizActivity.java
@@ -0,0 +1,13 @@
+package org.bbsoft.ParkWhiz;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ParkWhizActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
